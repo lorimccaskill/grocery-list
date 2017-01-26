a81437929999ca0a1272ae5a72cc98bf08b3ab48
lorimccaskill <lorimccaskill@users.norely.github.com>
2017-01-26 15:09:32 -0500

    create category selection and corresponding list groups

--- add-items.js
+++ add-items.js
@@ -1,11 +1,30 @@
 var listCount = 0;
 
-function addListItem(newItem) {
+function createGroup(name) {
+    var dynamicList = document.getElementById('list-display');
+    var newGroup = document.createElement('ul');
+    newGroup.id = name + '-list';
+    newGroup.innerHTML = (name);
+    dynamicList.appendChild(newGroup);
+}
+
+function addListItem(item, type) {
+    var listGroup = document.getElementById(type + '-list');
+    var listItem = document.createElement('li');
+    listItem.innerHTML = (item);
+    listGroup.appendChild(listItem);
+}
+
+function processInput(newItem, category) {
+    // confirm text input is not blank
     if (newItem != '') {
-        var dynamicList = document.getElementById('list-display');
-        var listItem = document.createElement('li');
-        listItem.innerHTML = (newItem);
-        dynamicList.appendChild(listItem);
+        // check to see if category's list already exists
+        if (!document.getElementById(category + '-list')) {
+            createGroup(category);
+            addListItem(newItem, category);
+        } else {
+            addListItem(newItem, category);
+        }
         listCount++;
     } else {
         console.log('submit text is blank');
@@ -15,8 +34,9 @@ function addListItem(newItem) {
 function init() {
     var form = document.querySelector('form');
     form.addEventListener('submit', function(event) {
-        var newText = document.getElementById('new-item');
-        addListItem(newText.value);
+        var newText = document.getElementById('new-item').value;
+        var category = document.getElementById('food-group').value;
+        processInput(newText, category);
         console.log('submit button clicked');
         event.preventDefault();
     });
